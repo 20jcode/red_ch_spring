2479f7fe86ad7f5bcc7fdbd0798e6bfeacd6b83f
mac <20j.code@gmail.com>
2024-08-30 16:27:04 +0900

    init

---
+++ src/main/java/com/redch/red_ch_spring/RedChSpringApplication.java
@@ -0,0 +1,13 @@
+package com.redch.red_ch_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedChSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedChSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redch/red_ch_spring/RedChSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redch.red_ch_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedChSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
